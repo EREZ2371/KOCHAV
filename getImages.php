cacf2b9260cf6151227252dada5f98a8d6e568aa
EREZ2371 <kochavhatzafon5@gmail.com>
2025-03-27 12:29:01 +0200

    Update getImages.php

--- getImages.php
+++ getImages.php
@@ -1,5 +1,18 @@
 <?php
-$dir = "images/";
-$images = glob($dir . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
-echo json_encode($images);
+$directory = 'images'; // שם התיקייה
+$images = [];
+
+// פתיחת תיקיית התמונות
+if ($handle = opendir($directory)) {
+    while (false !== ($entry = readdir($handle))) {
+        // אם קובץ הוא תמונה (תמונה בלבד לא תכיל נקודות כמו קבצים אחרים)
+        if (in_array(pathinfo($entry, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png', 'gif'])) {
+            $images[] = $directory . '/' . $entry; // הוספת שם הקובץ לרשימה
+        }
+    }
+    closedir($handle);
+}
+
+// החזרת התמונה כ- JSON
+echo json_encode(['images' => $images]);
 ?>
